55ef79d9956cabae0b65f6d8aef0ee8c6a53f311
Emylouly <emilylouisemika@gmail.com>
2024-03-06 21:09:32 -0300

    Update aluno.java

--- Vet/Vetor/src/aluno.java
+++ Vet/Vetor/src/aluno.java
@@ -1,9 +1,14 @@
+import java.util.Scanner;
+
 public class aluno {
 
-    public static <Scanner> void main(String[] args) {
+    public static void main(String[] args) {
         
         Scanner input = new Scanner(System.in);
+        String n;
 
-    }
-
+            n=input.nextLine();
+        
+ 
+}
 }
