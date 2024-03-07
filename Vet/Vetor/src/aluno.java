202aa6dfdcf34bcb821f6c7e7fd04d54ee1080fc
Emylouly <emilylouisemika@gmail.com>
2024-03-07 09:51:08 -0300

    Update aluno.java

--- Vet/Vetor/src/aluno.java
+++ Vet/Vetor/src/aluno.java
@@ -5,6 +5,8 @@ public class aluno {
     public static void main(String[] args) {
         
         Scanner input = new Scanner(System.in);
+
+        System.out.println("digite:");
         String n;
 
             n=input.nextLine();
